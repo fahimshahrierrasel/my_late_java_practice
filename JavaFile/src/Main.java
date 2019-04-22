89836ba06ba134856d9e3c87dbc6cd6173ddb9e1
Md. Fahim Shahrier Rasel <fahimshahrier2@gmail.com>
2019-04-22 22:44:09 +0600

    java file add

---
+++ JavaFile/src/Main.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.nio.file.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+//		fileWrite();
+		fileRead();
+	}
+
+	private static void fileWrite() {
+		Path file = Paths.get("Grades.txt");
+		String str = "ABCDF";
+		byte[] data = str.getBytes();
+		OutputStream output = null;
+		
+		try {
+			output = new BufferedOutputStream(Files.newOutputStream(file, StandardOpenOption.CREATE));
+			output.write(data);
+			output.flush();
+			output.close();
+		}
+		catch(Exception e) {
+			System.out.println("Message: " + e);
+		}
+	}
+	
+	private static void fileRead() {
+		Path file = Paths.get("Grades.txt");
+		InputStream input = null;
+		try {
+			input = Files.newInputStream(file);
+			BufferedReader reader  = new BufferedReader(new InputStreamReader(input));
+			String str = null;
+			str = reader.readLine();
+			System.out.println(str);
+			input.close();
+		}
+		catch(IOException e) {
+			System.out.println("Message: " + e);
+		}
+				
+	}
+}
